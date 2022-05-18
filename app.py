fb6dbed5abc614efc6565a20c372f57d8dd7ed58
Marcos8060 <marcosgav80@gmail.com>
2022-05-18 11:47:59 +0300

    my second kata

---
+++ app.py
@@ -0,0 +1,9 @@
+# taking user input and returning either odd or even
+
+userInput = int(input('enter a value '))
+if userInput % 2 == 0:
+    print('Your number is even')
+else:
+    print('your number is odd')
+
+print(userInput)
