a213d02c08e3bb7ceb1e9823a7b22b5079f5697c
Marcos8060 <marcosgav80@gmail.com>
2022-05-18 12:23:50 +0300

    fourth kata

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 // make a new array of only first and last element
-var a = [4,8,12,17,20]
+// var a = [4,8,12,17,20]
 
-let newArr = [a[0],a[4]]
-console.log(newArr);
+// let newArr = [a[0],a[4]]
+// console.log(newArr);
 
 
 // return a users number to start with 0 or +254 depending on the first digits of the number
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    const userText = document.getElementById('number').value
-    let newNumber = ''
-    if(userText.startsWith('0')){
-        newNumber = userText.replace('0','+254',1)
-    }else if(userText.startsWith('+254')){
-        newNumber = userText.replace('+254','0',1)
-    }
-    const result = document.getElementById('result')
-    result.innerHTML += `
-     <p>${newNumber}</p>
-    `
-})
+// form.addEventListener('submit',function(e){
+//     e.preventDefault()
+//     const userText = document.getElementById('number').value
+//     let newNumber = ''
+//     if(userText.startsWith('0')){
+//         newNumber = userText.replace('0','+254',1)
+//     }else if(userText.startsWith('+254')){
+//         newNumber = userText.replace('+254','0',1)
+//     }
+//     const result = document.getElementById('result')
+//     result.innerHTML += `
+//      <p>${newNumber}</p>
+//     `
+// })
+
+
+// check if user email contains @ or .
+// form.addEventListener('submit',function(e){
+//     e.preventDefault()
+
+//     const userEmail = document.getElementById('email').value
+//     if(userEmail.includes('@') && userEmail.includes('.')){
+//         alert('congratulations')
+//     }else{
+//         alert('we are sorry')
+//     }
+// })
